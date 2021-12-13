1f780dd598e202fe4f3bd2ee486a9f8806e8d72c
quangkt2812 <miniclip1213@gmail.com>
2021-12-14 00:07:23 +0700

    continue do book and fix css

--- src/java/dal/TicketDBContext.java
+++ src/java/dal/TicketDBContext.java
@@ -39,7 +39,60 @@ public class TicketDBContext extends DBContext {
     }
 
     public boolean insertTicket(int journeys_id, int train_id, String[] seats, int station_id, String[] prices, String username) {
-       
-    }
+        try {
+            String sql_payment = "INSERT INTO [dbo].[Payment]\n"
+                    + "           ([username])\n"
+                    + "     VALUES\n"
+                    + "           (?)";
+            String sql_ticket = "INSERT INTO [dbo].[Ticket]\n"
+                    + "           ([train_id]\n"
+                    + "           ,[row_number]\n"
+                    + "           ,[seat_number]\n"
+                    + "           ,[station_id]\n"
+                    + "           ,[journey_id]\n"
+                    + "           ,[price_id]\n"
+                    + "           ,[payment_id])\n"
+                    + "     VALUES\n"
+                    + "           (?\n"
+                    + "           ,?\n"
+                    + "           ,?\n"
+                    + "           ,?\n"
+                    + "           ,?\n"
+                    + "           ,?\n"
+                    + "           ,?)";
+            connection.setAutoCommit(true);
+            PreparedStatement stm_payment = connection.prepareStatement(sql_payment);
+            stm_payment.setString(1, username);
+            stm_payment.executeUpdate();
 
+            String sql_get_identity = "Select @@IDENTITY as payment_id";
+            PreparedStatement stm_get_identity = connection.prepareStatement(sql_get_identity);
+            ResultSet rs_get_identity = stm_get_identity.executeQuery();
+            int payment_id = 0;
+            if (rs_get_identity.next()) {
+                payment_id = rs_get_identity.getInt("payment_id");
+            }
+
+            for (int i = 0; i < seats.length; i++) {
+                PreparedStatement stm_ticket = connection.prepareStatement(sql_ticket);
+                stm_ticket.setInt(1, journeys_id);
+                stm_ticket.setInt(2, train_id);
+                stm_ticket.setInt(3, Integer.parseInt(seats[i]));
+                stm_ticket.setInt(4, station_id);
+                stm_ticket.setInt(5, Integer.parseInt(prices[i]));
+                stm_ticket.setInt(1, payment_id);
+                stm_ticket.executeUpdate();
+            }
+            connection.commit();
+        } catch (SQLException ex) {
+            Logger.getLogger(TicketDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+            } catch (SQLException ex) {
+                Logger.getLogger(TicketDBContext.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return true;
+    }
 }
